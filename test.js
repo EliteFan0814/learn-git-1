c7d35ac9bd9b862a1e43c6c59c8a64bbac906db9
范 培 超 <fan552426811@outlook.com>
2019-12-21 10:54:45 +0800

    test

--- test.js
+++ test.js
@@ -1,6 +1,4 @@
-export function getCookie(name) {
-  var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
-  arr = document.cookie.match(reg)
+export function getCookie(name) {var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");arr = document.cookie.match(reg)
   if (arr)
     return (arr[2]);
   else
