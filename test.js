8893a2be1003ea293471cdb1acc75c1b119b399e
codebug <fan552426811@outlook.com>
2019-12-21 11:34:24 +0800

    all

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
-export function getCookie(name) {var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");arr = document.cookie.match(reg)
+export function getCookie(name) {
+  var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+  arr = document.cookie.match(reg)
   if (arr)
     return (arr[2]);
   else
     console.log(222)
-    return null
+  return null
 }
 
 console.log(111)
