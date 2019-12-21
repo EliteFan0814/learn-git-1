d4c4a2bf8a6ae49833726fd56e91c6309c6e8d38
范 培 超 <fan552426811@outlook.com>
2019-12-21 10:50:16 +0800

    change row

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
-export function getCookie(name) {
-  var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+export function getCookie(name) {var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
   arr = document.cookie.match(reg)
   if (arr)
     return (arr[2]);
